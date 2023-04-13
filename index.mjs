b97b768fb9c185bb8afd440b648326e49843be84
littleforest <670177821@qq.com>
2023-04-13 17:47:54 +0800

    xxx

--- index.mjs
+++ index.mjs
@@ -1,4 +1,16 @@
 #! /usr/bin/env node
 
-const output = (await $`ls`).stdout.trim();
-console.log(output);
+// 使用git 自动保存代码
+// git add .
+// git commit -m "feat:asd";
+// git pull origin xxx;
+// git push;
+
+import { echo, $ } from "zx";
+
+await $`git add .`;
+const outputCommit = await $`git commit -m "xxx"`;
+echo(outputCommit);
+// const pull = await $`git pull origin master`;
+
+// const push = await $`git push origin master`;
