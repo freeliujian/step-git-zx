e3f78e75a3742b614f26604c9777c1e5c1b93aa4
littleforest <670177821@qq.com>
2023-04-14 09:27:03 +0800

    feat: add zx

--- auto-git-save.mjs
+++ auto-git-save.mjs
@@ -1,15 +1,8 @@
 #! /usr/bin/env node
 
-// 使用git 自动保存代码
-// git add .
-// git commit -m "feat:asd";
-// git pull origin xxx;
-// git push;
-
 import { echo, $ } from "zx";
 
 await $`git add .`;
 const outputCommit = await $`git commit -m "xxx"`;
 const pull = await $`git pull origin master`;
-
-// const push = await $`git push origin master`;
+const push = await $`git push origin master`;
---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+import { program } from "commander";
+
+program.
+  name('test').
+  
+
+program.parse()
+
