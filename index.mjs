854dcc4d198e31b5eeb24d30012d4695383f009b
littleforest <670177821@qq.com>
2023-04-17 16:44:53 +0800

    $'添加一些新的指令'

--- index.mjs
+++ index.mjs
@@ -3,8 +3,8 @@
 import { program } from "commander";
 import { $ } from 'zx';
 
-const autoGit = async (branchName, commitMessage) => {
-    await $`git add .`;
+const autoGit = async (branchName, commitMessage, fileName) => {
+    await $`git add ${(fileName)?fileName:'.'}`;
     await $`git commit -m "${commitMessage}"`;
     await $`git pull origin ${(branchName)?branchName:'master'}`;
     await $`git push origin  ${(branchName)?branchName:'master'}`;
@@ -13,18 +13,17 @@ const autoGit = async (branchName, commitMessage) => {
 program.
   name('gitSubmit').
   description('使用zx写git的shell脚本自动push代码').
-  option('-g,  --git  [commitMessage]').
+  option('-f --file [fileName]').
+  option('-g,  --git  <commitMessage>').
   option('-b --branch [branchName').
-  action(async (value) => {
-      console.log(value)
-    if (value.git) {
-      autoGit(value?.branchName, value.git);
+  argument('[ls]').
+  action(async (_value, output) => {
+    console.log(output)
+    if (output.git) {
+     autoGit(output?.branchName, output.git, output.file);
     }
-  })
-
-  // program.name('')
-
-  
-
-program.parse();
+    return
+  });
+   
+program.parse(process.argv)
 
