16d9642c032ea8df0146c15bcb1f9b42ec39434e
littleforest <670177821@qq.com>
2023-04-17 16:15:59 +0800

    woshidashuaibi

--- index.mjs
+++ index.mjs
@@ -3,18 +3,27 @@
 import { program } from "commander";
 import { $ } from 'zx';
 
+const autoGit = async (branchName, commitMessage) => {
+    await $`git add .`;
+    await $`git commit -m "${commitMessage}"`;
+    await $`git pull origin ${(branchName)?branchName:'master'}`;
+    await $`git push origin  ${(branchName)?branchName:'master'}`;
+};
+
 program.
-  name('test').
-  option('-g --git [commitMessage]').
+  name('gitSubmit').
+  description('使用zx写git的shell脚本自动push代码').
+  option('-g,  --git  [commitMessage]').
+  option('-b --branch [branchName').
   action(async (value) => {
-    console.log(value)
+      console.log(value)
     if (value.git) {
-      await $`git add .`;
-      await $`git commit -m "${value.git}"`;
-      await $`git pull origin master`;
-      await $`git push origin master`;
+      autoGit(value?.branchName, value.git);
     }
   })
+
+  // program.name('')
+
   
 
 program.parse();
