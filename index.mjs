0ed104169f0ec72955eba9b25265352c1001a220
littleforest <670177821@qq.com>
2023-04-18 09:58:39 +0800

    test

--- index.mjs
+++ index.mjs
@@ -4,10 +4,10 @@ import { program } from "commander";
 import { $ } from 'zx';
 
 const autoGit = async (branchName, commitMessage, fileName) => {
-    await $`git add ${(fileName)?fileName:'.'}`;
+    await $`git add ${fileName}`;
     await $`git commit -m "${commitMessage}"`;
-    await $`git pull origin ${(branchName)?branchName:'master'}`;
-    await $`git push origin  ${(branchName)?branchName:'master'}`;
+    await $`git pull origin ${branchName}`;
+    await $`git push origin  ${branchName}`;
 };
 
 program.
@@ -15,15 +15,22 @@ program.
   description('使用zx写git的shell脚本自动push代码').
   option('-f --file [fileName]').
   option('-g,  --git  <commitMessage>').
-  option('-b --branch [branchName').
+  option('-b --branch [branchName]').
   argument('[ls]').
-  action(async (_value, output) => {
-    console.log(output)
-    if (output.git) {
-     autoGit(output?.branchName, output.git, output.file);
+
+  action(async (value, option) => {
+    console.log(value,option)
+    if (option.git) {
+      const { git, branchName = 'master', file = '.' } = option;
+      autoGit(branchName, git, file);
+    }
+    if (value) {
+      $`${value}`
     }
     return
   });
    
 program.parse(process.argv)
+
+
 
