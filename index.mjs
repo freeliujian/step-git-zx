8b3e70365add198ea407ad7d143f9045c46919e3
littleforest <670177821@qq.com>
2023-04-20 14:25:02 +0800

    commit messgae

--- index.mjs
+++ index.mjs
@@ -1,34 +1,6 @@
-#! /usr/bin/env node
+#! /usr/bin/env zx
 
-import { program } from "commander";
-import pkg  from './package.json' assert { type: 'json' };
-import { $ } from 'zx';
-
-const autoGit = async (branchName, commitMessage, fileName) => {
-    await $`git add ${fileName}`;
-    await $`git commit -m "${commitMessage}"`;
-    await $`git pull origin ${branchName}`;
-    await $`git push origin  ${branchName}`;
-};
-
-program.version(pkg.version);
-
-program.
-  name('gitSubmit').
-  description('使用zx写git的shell脚本自动push代码').
-  command('git').
-  argument('<branchName>', 'branch name').
-  argument('<commitMessage>', 'commit message').
-  argument('<fileName>', 'file name').
-  //arguments('<branchName>|<commitMessage>|<fileName>').
-  action((branchName, commitMessage, fileName) => {
-    if (branchName, commitMessage, fileName) {
-      autoGit(branchName, commitMessage, fileName)
-    }
-  })
-
-
-program.parse(process.argv);
-
-
-
+await $`git add .`;
+await $`git commit -m "commit messgae"`;
+await $`git pull origin master`;
+await $`git push origin  master`;
